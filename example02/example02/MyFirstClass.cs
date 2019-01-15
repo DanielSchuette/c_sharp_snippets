826c8860662498db66bf7a31bb9c0e77980cebbe
DanielSchuette <d.schuette@online.de>
2019-01-16 00:04:27 +0100

    add example02

---
+++ example02/example02/MyFirstClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace example02
+{
+    /* a book data type */
+    public class MyFirstClass
+    {
+        public MyFirstClass()
+        {
+        }
+        /* attributes */
+        public string title;
+        public string author;
+        public int pages;
+    }
+}
---
+++ example02/example02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace example02
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            /* one way to instantiate an object */
+            MyFirstClass book1 = new MyFirstClass();
+            book1.title = "my book";
+            book1.author = "me";
+            book1.pages = 42; /* what else? */
+
+            /* another way to instantiate an object */
+            MyFirstClass book2 = new MyFirstClass
+            {
+                title = "my second book",
+                author = "me (of course)",
+                pages = 43,
+            };
+            Console.WriteLine(book1.title);
+            Console.WriteLine(book2.author);
+        }
+    }
+}
